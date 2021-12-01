eb2311df56b190b6841e1fb0aead7ba7ba89d5b9
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-12-01 17:35:28 -0300

    Passing Tests

---
+++ src/main/java/com/devsuperior/bds02/services/exceptions/DataBaseException.java
@@ -0,0 +1,15 @@
+package com.devsuperior.bds02.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+	
+	public DataBaseException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
